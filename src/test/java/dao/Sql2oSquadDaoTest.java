8dc03df70c4944852fb7eaa6cd910cdbf629499f
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-22 21:55:31 +0200

    testest the squadDao

--- src/test/java/dao/Sql2oSquadDaoTest.java
+++ src/test/java/dao/Sql2oSquadDaoTest.java
@@ -1,7 +1,110 @@
 package dao;
 
+import models.Squad;
+import models.Hero;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 import static org.junit.Assert.*;
 
+
 public class Sql2oSquadDaoTest {
+    private Sql2oSquadDao squadDao;
+    private Sql2oHeroDao heroDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        squadDao = new Sql2oSquadDao(sql2o);
+        heroDao = new Sql2oHeroDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void addingCategorySetsId() throws Exception {
+        Squad squad = setupNewSquad();
+        int originalSquadId = squad.getId();
+        squadDao.add(squad);
+        assertNotEquals(originalSquadId, squad.getId());
+    }
+
+    @Test
+    public void existingCategoriesCanBeFoundById() throws Exception {
+        Squad squad = setupNewSquad();
+        squadDao.add(squad);
+        Squad foundSquad = squadDao.findById(squad.getId());
+        assertEquals(squad, foundSquad);
+    }
+
+    @Test
+    public void addedCategoriesAreReturnedFromGetAll() throws Exception {
+        Squad squad = setupNewSquad();
+        squadDao.add(squad);
+        assertEquals(1, squadDao.getAll().size());
+    }
+
+    @Test
+    public void noCategoriesReturnsEmptyList() throws Exception {
+        assertEquals(0, squadDao.getAll().size());
+    }
+
+    @Test
+    public void updateChangesCategoryContent() throws Exception {
+        String initialName = "worker";
+        Squad squad = new Squad (initialName);
+        squadDao.add(squad);
+        squadDao.update(squad.getId(),"Cleaning");
+        Squad updatedSquad = squadDao.findById(squad.getId());
+        assertNotEquals(initialName, updatedSquad.getName());
+    }
+
+    @Test
+    public void deleteByIdDeletesCorrectCategory() throws Exception {
+        Squad squad = setupNewSquad();
+        squadDao.add(squad);
+        squadDao.deleteById(squad.getId());
+        assertEquals(0, squadDao.getAll().size());
+    }
+
+    @Test
+    public void clearAllClearsAllCategories() throws Exception {
+        Squad squad = setupNewSquad();
+        Squad otherSquad= new Squad("Cleaning");
+        squadDao.add(squad);
+        squadDao.add(otherSquad);
+        int daoSize = squadDao.getAll().size();
+        squadDao.clearAllSquads();
+        assertTrue(daoSize > 0 && daoSize > squadDao.getAll().size());
+    }
+
+    @Test
+    public void getAllTasksByCategoryReturnsTasksCorrectly() throws Exception {
+        Squad squad = setupNewSquad();
+        squadDao.add(squad);
+        int squadId = squad.getId();
+        Hero newHero = new Hero("Lucky", squadId);
+        Hero otherHero = new Hero("Pauline", squadId);
+        Hero thirdHero = new Hero("Hedgar", squadId);
+        heroDao.add(newHero);
+        heroDao.add(otherHero); //we are not adding task 3 so we can test things precisely.
+        assertEquals(2, squadDao.getAllHeroesBySquad(squadId).size());
+        assertTrue(squadDao.getAllHeroesBySquad(squadId).contains(newHero));
+        assertTrue(squadDao.getAllHeroesBySquad(squadId).contains(otherHero));
+        assertFalse(squadDao.getAllHeroesBySquad(squadId).contains(thirdHero)); //things are accurate!
+    }
+
+    // helper method
+    public Squad setupNewSquad(){
+        return new Squad("worker");
+    }
 
 }
